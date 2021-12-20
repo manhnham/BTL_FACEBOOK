02067cf6c3cf9bb84df63f5ba25eef031fce109b
manhnham <94447961+manhnham@users.noreply.github.com>
2021-12-20 21:44:06 +0700

    Add files via upload

--- trangchinh.php
+++ trangchinh.php
@@ -121,38 +121,320 @@
             <div class="storyReel">
                 <div style="background-image:url('https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg/220px-Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg') "
                     class="story">
-                    <img class="user_avatar story_avatar" src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
+                    <img class="user_avatar story_avatar"
+                        src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
                         alt="">
                     <h4>antada</h4>
                 </div>
                 <div style="background-image:url('https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg/220px-Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg') "
                     class="story">
-                    <img class="user_avatar story_avatar" src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
-                        alt="">
-                        <h4>atada</h4>
-                </div>
-                <div style="background-image:url('https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg/220px-Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg') "
-                    class="story">
-                    <img class="user_avatar story_avatar" src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
+                    <img class="user_avatar story_avatar"
+                        src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
                         alt="">
                     <h4>atada</h4>
                 </div>
                 <div style="background-image:url('https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg/220px-Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg') "
                     class="story">
-                    <img class="user_avatar story_avatar" src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
+                    <img class="user_avatar story_avatar"
+                        src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
                         alt="">
                     <h4>atada</h4>
                 </div>
                 <div style="background-image:url('https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg/220px-Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg') "
                     class="story">
-                    <img class="user_avatar story_avatar" src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
+                    <img class="user_avatar story_avatar"
+                        src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
+                        alt="">
+                    <h4>atada</h4>
+                </div>
+                <div style="background-image:url('https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg/220px-Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg') "
+                    class="story">
+                    <img class="user_avatar story_avatar"
+                        src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
                         alt="">
                     <h4>atada</h4>
                 </div>
             </div>
-        </div>
-    </div>
+            <!--message-->
+            <div class="messageSender">
+                <div class="messageSender_top">
+                    <img class="user_avatar story_avatar"
+                        src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
+                        alt="">
+                    <form>
+                        <input type=" text" class="messageSender_input" placeholder="Anata ơi,bạn đang nghĩ gì thế?">
+                    </form>
+                </div>
+                <div class="messageSender_bottom">
+                    <div class="messageSender_option">
+                        <span style="color: red" class="material-icons">
+                            videocam
+                        </span>
+                        <h3>Video trực tiếp</h3>
+                    </div>
+                    <div class="messageSender_option">
+                        <span style="color: green" class="material-icons">
+                            photo_library
+                        </span>
+                        <h3>Ảnh/Video</h3>
+                    </div>
+                    <div class="messageSender_option">
+                        <span style="color: yellow" class="material-icons">
+                            insert_emoticon
+                        </span>
+                        <h3>Cảm xúc/Hoạt động</h3>
+                    </div>
+                </div>
+            </div>
+            <!--post-->
+            <div class="post">
+                <div class="post_top">
+                    <img class="user_avatar post_avatar"
+                        src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
+                        alt="">
+                    <div class="post_topInfo">
+                        <h3>anata</h3>
+                        <p>28 April at 20:30</p>
+                    </div>
+                </div>
+                <div class="post_bottom">
+                    <p>Message</p>
+                </div>
+                <div class="post_image">
+                    <img src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
+                        alt="">
 
+                </div>
+                <div class="post_options">
+                    <div class="post_option">
+                        <span class="material-icons">
+                            thumb_up
+                        </span>
+                        <p>Like</p>
+                    </div>
+                    <div class="post_option">
+                        <span class="material-icons">
+                            chat_bubble_outline
+                        </span>
+                        <p>Comment</p>
+                    </div>
+                    <div class="post_option">
+                        <span class="material-icons">
+                            near_me
+                        </span>
+                        <p>Share</p>
+                    </div>
+                </div>
+            </div>
+            <div class="post">
+                <div class="post_top">
+                    <img class="user_avatar post_avatar"
+                        src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
+                        alt="">
+                    <div class="post_topInfo">
+                        <h3>anata</h3>
+                        <p>28 April at 20:30</p>
+                    </div>
+                </div>
+                <div class="post_bottom">
+                    <p>Message</p>
+                </div>
+                <div class="post_image">
+                    <img src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
+                        alt="">
+
+                </div>
+                <div class="post_options">
+                    <div class="post_option">
+                        <span class="material-icons">
+                            thumb_up
+                        </span>
+                        <p>Like</p>
+                    </div>
+                    <div class="post_option">
+                        <span class="material-icons">
+                            chat_bubble_outline
+                        </span>
+                        <p>Comment</p>
+                    </div>
+                    <div class="post_option">
+                        <span class="material-icons">
+                            near_me
+                        </span>
+                        <p>Share</p>
+                    </div>
+                </div>
+            </div>
+            <div class="post">
+                <div class="post_top">
+                    <img class="user_avatar post_avatar"
+                        src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
+                        alt="">
+                    <div class="post_topInfo">
+                        <h3>anata</h3>
+                        <p>28 April at 20:30</p>
+                    </div>
+                </div>
+                <div class="post_bottom">
+                    <p>Message</p>
+                </div>
+                <div class="post_image">
+                    <img src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
+                        alt="">
+
+                </div>
+                <div class="post_options">
+                    <div class="post_option">
+                        <span class="material-icons">
+                            thumb_up
+                        </span>
+                        <p>Like</p>
+                    </div>
+                    <div class="post_option">
+                        <span class="material-icons">
+                            chat_bubble_outline
+                        </span>
+                        <p>Comment</p>
+                    </div>
+                    <div class="post_option">
+                        <span class="material-icons">
+                            near_me
+                        </span>
+                        <p>Share</p>
+                    </div>
+                </div>
+            </div>
+            <div class="post">
+                <div class="post_top">
+                    <img class="user_avatar post_avatar"
+                        src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
+                        alt="">
+                    <div class="post_topInfo">
+                        <h3>anata</h3>
+                        <p>28 April at 20:30</p>
+                    </div>
+                </div>
+                <div class="post_bottom">
+                    <p>Message</p>
+                </div>
+                <div class="post_image">
+                    <img src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
+                        alt="">
+
+                </div>
+                <div class="post_options">
+                    <div class="post_option">
+                        <span class="material-icons">
+                            thumb_up
+                        </span>
+                        <p>Like</p>
+                    </div>
+                    <div class="post_option">
+                        <span class="material-icons">
+                            chat_bubble_outline
+                        </span>
+                        <p>Comment</p>
+                    </div>
+                    <div class="post_option">
+                        <span class="material-icons">
+                            near_me
+                        </span>
+                        <p>Share</p>
+                    </div>
+                </div>
+            </div>
+            <div class="post">
+                <div class="post_top">
+                    <img class="user_avatar post_avatar"
+                        src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
+                        alt="">
+                    <div class="post_topInfo">
+                        <h3>anata</h3>
+                        <p>28 April at 20:30</p>
+                    </div>
+                </div>
+                <div class="post_bottom">
+                    <p>Message</p>
+                </div>
+                <div class="post_image">
+                    <img src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
+                        alt="">
+
+                </div>
+                <div class="post_options">
+                    <div class="post_option">
+                        <span class="material-icons">
+                            thumb_up
+                        </span>
+                        <p>Like</p>
+                    </div>
+                    <div class="post_option">
+                        <span class="material-icons">
+                            chat_bubble_outline
+                        </span>
+                        <p>Comment</p>
+                    </div>
+                    <div class="post_option">
+                        <span class="material-icons">
+                            near_me
+                        </span>
+                        <p>Share</p>
+                    </div>
+                </div>
+            </div>
+            <div class="post">
+                <div class="post_top">
+                    <img class="user_avatar post_avatar"
+                        src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
+                        alt="">
+                    <div class="post_topInfo">
+                        <h3>anata</h3>
+                        <p>28 April at 20:30</p>
+                    </div>
+                </div>
+                <div class="post_bottom">
+                    <p>Message</p>
+                </div>
+                <div class="post_image">
+                    <img src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
+                        alt="">
+
+                </div>
+                <div class="post_options">
+                    <div class="post_option">
+                        <span class="material-icons">
+                            thumb_up
+                        </span>
+                        <p>Like</p>
+                    </div>
+                    <div class="post_option">
+                        <span class="material-icons">
+                            chat_bubble_outline
+                        </span>
+                        <p>Comment</p>
+                    </div>
+                    <div class="post_option">
+                        <span class="material-icons">
+                            near_me
+                        </span>
+                        <p>Share</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div style="flex:0.33" class="widgets">
+            <div class="fb-page" data-href="https://www.facebook.com/facebook" data-tabs="timeline" data-width="500"
+                data-height="1000" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false"
+                data-show-facepile="true">
+                <blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a
+                        href="https://www.facebook.com/facebook">Meta</a></blockquote>
+            </div>
+        </div>
+
+    </div>
+    <div id="fb-root"></div>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v12.0"
+        nonce="UWE7B9tW"></script>
 </body>
 
 </html>
