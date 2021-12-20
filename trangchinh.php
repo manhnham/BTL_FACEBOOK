547401ea78b6dda4b651c3c354cdc1d785458df4
manhnham <94447961+manhnham@users.noreply.github.com>
2021-12-20 20:09:18 +0700

    Add files via upload

---
+++ trangchinh.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Facebook</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="trangchinh.css">
+
+</head>
+
+<body>
+    <div class="header">
+        <div class="header_left">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1200px-Facebook_Logo_%282019%29.png"
+                alt="">
+            <div class="header_input">
+                <span class="material-icons">
+                    search
+                </span>
+                <input type="text" placeholder="Tìm kiếm trên Facebook">
+            </div>
+        </div>
+
+
+        <div class="header_middle">
+            <div class="header_option active">
+                <span class="material-icons">
+                    home
+                </span>
+            </div>
+            <div class="header_option">
+                <span class="material-icons">
+                    people
+                </span>
+            </div>
+            <div class="header_option">
+
+                <span class="material-icons">
+                    smart_display
+                </span>
+            </div>
+            <div class="header_option">
+                <span class="material-icons">
+                    storefront
+                </span>
+            </div>
+
+            <div class="header_option">
+                <span class="material-icons">
+                    supervised_user_circle
+                </span>
+            </div>
+        </div>
+        <div class="header_right">
+            <div class="header_info">
+                <img class="user_avatar"
+                    src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
+                    alt="">
+                <h4>Anata Mạnh</h4>
+                <span class="material-icons">
+                    apps
+                </span>
+                <span class="material-icons">
+                    chat
+                </span>
+                <span class="material-icons">
+                    notifications
+                </span>
+            </div>
+        </div>
+    </div>
+    <div class="main_body">
+        <div class="sidebar">
+            <div class="sidebarRow">
+                <img class="user_avatar"
+                    src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
+                    alt="">
+                <h4>Anata Mạnh</h4>
+            </div>
+            <div class="sidebarRow">
+                <span class="material-icons">
+                    people
+                </span>
+                <h4>Bạn Bè</h4>
+            </div>
+            <div class="sidebarRow">
+                <span class="material-icons">
+                    supervised_user_circle
+                </span>
+                <h4>Nhóm</h4>
+            </div>
+            <div class="sidebarRow">
+                <span class="material-icons">
+                    storefront
+                </span>
+                <h4>MarketPlace</h4>
+            </div>
+            <div class="sidebarRow">
+                <span class="material-icons">
+                    smart_display
+                </span>
+                <h4>Watch</h4>
+            </div>
+            <div class="sidebarRow">
+                <span class="material-icons">
+                    history
+                </span>
+                <h4>Kỷ niệm</h4>
+            </div>
+            <div class="sidebarRow">
+                <span class="material-icons">
+                    expand_more
+                </span>
+                <h4>Xem thêm</h4>
+            </div>
+        </div>
+        <div class="feed">
+            <div class="storyReel">
+                <div style="background-image:url('https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg/220px-Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg') "
+                    class="story">
+                    <img class="user_avatar story_avatar" src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
+                        alt="">
+                    <h4>antada</h4>
+                </div>
+                <div style="background-image:url('https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg/220px-Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg') "
+                    class="story">
+                    <img class="user_avatar story_avatar" src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
+                        alt="">
+                        <h4>atada</h4>
+                </div>
+                <div style="background-image:url('https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg/220px-Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg') "
+                    class="story">
+                    <img class="user_avatar story_avatar" src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
+                        alt="">
+                    <h4>atada</h4>
+                </div>
+                <div style="background-image:url('https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg/220px-Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg') "
+                    class="story">
+                    <img class="user_avatar story_avatar" src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
+                        alt="">
+                    <h4>atada</h4>
+                </div>
+                <div style="background-image:url('https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg/220px-Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg') "
+                    class="story">
+                    <img class="user_avatar story_avatar" src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/245265801_1782202625303869_6260711662080745064_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=HB6J6llm5G8AX91abHo&_nc_ht=scontent-sin6-2.xx&oh=00_AT9Qc04ogcaEl4_lO8VoOY_L4tvbOE1IZ0KH-8U0R-8Yyg&oe=61C45BDF"
+                        alt="">
+                    <h4>atada</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
