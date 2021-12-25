e76f6997a4cb80abc21e8f1bb76c0837915a57aa
hoang10adt <hoangnguyenviet13012002@gmail.com>
2021-12-25 22:22:42 +0700

    test

--- process.php
+++ process.php
@@ -24,35 +24,35 @@ if(isset($_POST['btnRegister']) && $_POST['email'])
         }else{
             echo"Xin lỗi email chưa đc gửi vui lòng kiểm tra lại";
         }
-        // require_once('phpmail/PHPMailerAutoload.php');
-        // $mail = new PHPMailer();
-        // $mail->CharSet =  "utf-8";
-        // $mail->IsSMTP();
-        // // enable SMTP authentication
-        // $mail->SMTPAuth = true;                  
-        // // GMAIL username
-        // $mail->Username = "your_email_id@gmail.com";
-        // // GMAIL password
-        // $mail->Password = "your_gmail_password";
-        // $mail->SMTPSecure = "ssl";  
-        // // sets GMAIL as the SMTP server
-        // $mail->Host = "smtp.gmail.com";
-        // // set the SMTP port for the GMAIL server
-        // $mail->Port = "465";
-        // $mail->From='your_gmail_id@gmail.com';
-        // $mail->FromName='your_name';
-        // $mail->AddAddress('reciever_email_id', 'reciever_name');
-        // $mail->Subject  =  'Reset Password';
-        // $mail->IsHTML(true);
-        // $mail->Body    = 'Click On This Link to Verify Email '.$link.'';
-        // if($mail->Send())
-        // {
-        // echo "Check Your Email box and Click on the email verification link.";
-        // }
-        // else
-        // {
-        // echo "Mail Error - >".$mail->ErrorInfo;
-        // }
+        require_once('PHPMailer/PHPMailerAutoload.php');
+        $mail = new PHPMailer();
+        $mail->CharSet =  "utf-8";
+        $mail->IsSMTP();
+        // enable SMTP authentication
+        $mail->SMTPAuth = true;                  
+        // GMAIL username
+        $mail->Username = "your_email_id@gmail.com";
+        // GMAIL password
+        $mail->Password = "your_gmail_password";
+        $mail->SMTPSecure = "ssl";  
+        // sets GMAIL as the SMTP server
+        $mail->Host = "smtp.gmail.com";
+        // set the SMTP port for the GMAIL server
+        $mail->Port = "465";
+        $mail->From='your_gmail_id@gmail.com';
+        $mail->FromName='your_name';
+        $mail->AddAddress('reciever_email_id', 'reciever_name');
+        $mail->Subject  =  'Reset Password';
+        $mail->IsHTML(true);
+        $mail->Body    = 'Click On This Link to Verify Email '.$link.'';
+        if($mail->Send())
+        {
+        echo "Check Your Email box and Click on the email verification link.";
+        }
+        else
+        {
+        echo "Mail Error - >".$mail->ErrorInfo;
+        }
     }
     else
     {
